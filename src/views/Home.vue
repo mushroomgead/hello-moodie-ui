eef76a9dbb3a3fa18f8234fb6de7e0c50a909420
Kate <mushroom.gead@gmail.com>
2021-04-17 06:41:17 +0700

    init commit

---
+++ src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <h1>Overview {{ title }}</h1>
+  <div class="container-body"></div>
+  <span v-for="item in items" :key="item.id" :load="log(item)">
+    {{ item.username }}
+  </span>
+  <button v-on:click="handleOnAdd">add mood today</button>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Home",
+  data() {
+    return {
+      items: [],
+    };
+  },
+  async created() {
+    let result = await axios.get("http://localhost:4000/user");
+    this.items = result.data;
+  },
+  methods: {
+    log(item) {
+      console.log("item: ", item);
+    },
+    handleOnAdd() {
+      console.log("click item");
+
+      this.$router.push("about");
+    },
+  },
+};
+</script>
+
+<style scoped></style>
